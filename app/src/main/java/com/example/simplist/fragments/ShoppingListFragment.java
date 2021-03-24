eb32457f1f0af47b82de7c0296d776634c7d61a2
Kevin Hansen <kevin.lhansen@hotmail.com>
2021-03-24 11:47:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/simplist/fragments/ShoppingListFragment.java
@@ -0,0 +1,11 @@
+package com.example.simplist.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.simplist.R;
+
+public class ShoppingListFragment extends Fragment {
+    public ShoppingListFragment() {
+        super(R.layout.shoppinglistfragment);
+    }
+}
