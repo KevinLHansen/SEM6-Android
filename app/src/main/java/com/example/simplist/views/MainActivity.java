ffa34754ce28ba3ff7339cf9bf04d879ba59d99c
IIsSchulstad <sjoeholm.rs@gmail.com>
2021-03-24 17:57:41 +0100

    Observer works

--- app/src/main/java/com/example/simplist/views/MainActivity.java
+++ app/src/main/java/com/example/simplist/views/MainActivity.java
@@ -1,24 +1,18 @@
 package com.example.simplist.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-
 import com.example.simplist.R;
-import com.example.simplist.models.ShoppingList;
 import com.example.simplist.viewmodels.ShoppingListViewModel;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    MainActivity context;
     RecyclerView recyclerView;
-    ShoppingListListAdapter adapter;
+    SLListAdapter adapter;
     ShoppingListViewModel slvm;
 
     @Override
@@ -26,26 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = this;
-
         slvm = new ViewModelProvider(this).get(ShoppingListViewModel.class);
+        adapter = new SLListAdapter();
 
         recyclerView = findViewById(R.id.shoppingListList);
-        ShoppingListListAdapter adapter = new ShoppingListListAdapter(this, slvm.getLists().getValue());
-        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        slvm.getLists().observe(this, shoppingListUpdateObserver);
-
+        recyclerView.setAdapter(adapter);
+        slvm.getLists().observe(this, list -> {
+            adapter.setLists(list);
+        });
     }
-
-    Observer<ArrayList<ShoppingList>> shoppingListUpdateObserver = new Observer<ArrayList<ShoppingList>>() {
-
-        @Override
-        public void onChanged(ArrayList<ShoppingList> shoppingLists) {
-            adapter = new ShoppingListListAdapter(context, shoppingLists);
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            recyclerView.setAdapter(adapter);
-        }
-    };
 }
--- app/src/main/java/com/example/simplist/views/SLListAdapter.java
+++ app/src/main/java/com/example/simplist/views/SLListAdapter.java
@@ -1,36 +1,32 @@
 package com.example.simplist.views;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.simplist.R;
 import com.example.simplist.models.ShoppingList;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class ShoppingListListAdapter extends RecyclerView.Adapter<ShoppingListListAdapter.ShoppingListListViewHolder> {
+public class SLListAdapter extends RecyclerView.Adapter<SLListAdapter.ShoppingListListViewHolder> {
 
-    Context context;
-    ArrayList<ShoppingList> shoppingLists;
+    private ArrayList<ShoppingList> shoppingLists;
 
-    public ShoppingListListAdapter(Context context, ArrayList<ShoppingList> shoppingLists) { // INSERT DATA FROM DATABASE IN ARGS
-        this.context = context;
+    public void setLists(ArrayList<ShoppingList> shoppingLists){
         this.shoppingLists = shoppingLists;
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public ShoppingListListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.shopping_list, parent, false);
         return new ShoppingListListViewHolder(view);
     }
